054df09cc80e8ef1e9eb75f7b903838f2f4ba786
guribaby <122307456+guribaby@users.noreply.github.com>
2023-05-04 22:03:59 -0600

    Create programa.java

---
+++ programa.java
@@ -0,0 +1,20 @@
+package act15;
+
+
+
+public class programa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		matriz z = new matriz ();
+		
+		
+		
+		System.out.println(z.multiMatriz ());
+		
+		
+		
+	}
+
+}
